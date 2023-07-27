7c8a1404ba843d5caeef3a1c518a7ca1f315d9d1
Raja Junaid <rajajunaidpdk@gmail.com>
2023-07-27 13:23:25 +0500

    Beautify my code using Prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ['./*.html'],
   theme: {
     extend: {
       colors: {
-        White: "#ffffff",
-        Black: "#000000",
-        darkBlue1: "hsl(217, 28%, 15%)",
-        darkBlueMain: "hsl(218, 28%, 13%)",
-        darkBlueFooter: "hsl(216, 53%, 9%)",
-        darkBlueTesti: "hsl(219, 30%, 18%)",
-        Cyan: "hsl(176, 68%, 64%)",
-        Blue: "hsl(198, 60%, 50%)",
-        lightRed: "hsl(0, 100%, 63%)",
+        White: '#ffffff',
+        Black: '#000000',
+        Grey: '#dddddd',
+        darkBlue1: 'hsl(217, 28%, 15%)',
+        darkBlueMain: 'hsl(218, 28%, 13%)',
+        darkBlueFooter: 'hsl(216, 53%, 9%)',
+        darkBlueTesti: 'hsl(219, 30%, 18%)',
+        Cyan: 'hsl(176, 68%, 64%)',
+        Blue: 'hsl(198, 60%, 50%)',
+        lightRed: 'hsl(0, 100%, 63%)',
       },
 
       gridTemplateColumns: {
-        custom: "1fr 1fr",
+        custom: '1fr 1fr',
       },
     },
   },
